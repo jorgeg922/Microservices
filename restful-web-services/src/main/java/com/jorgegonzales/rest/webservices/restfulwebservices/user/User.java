0582f0369f1f62d2f5597b38769041e12f5a0fa6
jorge <jorge@DESKTOP-90LHD9V>
2024-02-25 09:31:43 -0800

    Filtering methods

--- restful-web-services/src/main/java/com/jorgegonzales/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/jorgegonzales/rest/webservices/restfulwebservices/user/User.java
@@ -2,14 +2,17 @@ package com.jorgegonzales.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 public class User {
 	private Integer id;
-	
+	@JsonProperty("user_name")
 	@Size(min=2, message = "Name should have at least 2 characters")
 	private String name;
+	@JsonProperty("birth_date")
 	@Past(message = "Birth date should be in the past")
 	private LocalDate birthDate;
 	
--- restful-web-services/src/main/java/com/jorgegonzales/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/jorgegonzales/rest/webservices/restfulwebservices/user/UserResource.java
@@ -5,6 +5,9 @@ package com.jorgegonzales.rest.webservices.restfulwebservices.user;
 import java.net.URI;
 import java.util.List;
 
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+//import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 
 import jakarta.validation.Valid;
 
@@ -27,14 +31,20 @@ public class UserResource {
 	public List<User> retrieveAllUsers(){
 		return service.findAll();
 	}
+	//http://localhost:8080/users
+	//Entity Model
+	//WebMvcLinkBuilder
 	
 	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable Integer id){
+	public EntityModel<User> retrieveUser(@PathVariable Integer id){
 		User user = service.findOne(id);
 		if(user==null) {
 			throw new UserNotFoundException("id:"+id);
 		}
-		return user;
+		EntityModel<User> entityModel = EntityModel.of(user);
+		WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		entityModel.add(link.withRel("all-users"));
+		return entityModel;
 	}
 	
 	@PostMapping("/users")
