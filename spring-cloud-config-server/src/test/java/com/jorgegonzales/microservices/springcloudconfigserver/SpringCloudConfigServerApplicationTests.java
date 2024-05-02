50216b3bf1dba32ffa4523a236bbf30bfc903d79
jorge <jorge@DESKTOP-90LHD9V>
2024-05-02 07:39:06 -0700

    config server

---
+++ spring-cloud-config-server/src/test/java/com/jorgegonzales/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorgegonzales.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
